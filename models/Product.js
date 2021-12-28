6ddb698ebf31bb18a7eab03a64c5eeeb520b6851
Marek Gogoľ <info@marekgogol.sk>
2021-12-28 21:51:14 +0100

    merge fix

--- models/Product.js
+++ models/Product.js
@@ -233,35 +233,6 @@ class Product extends BaseProduct {
 
         return filtratedVariants[0];
     }
-
-    /**
-     * Variants
-     */
-    async toggleFavourite() {
-        try {
-            let response = await $nuxt.$axios.$post(
-                $nuxt.$action('FavouriteController@toggleFavourite'),
-                {
-                    product_id: this.id,
-                }
-            );
-
-            crudadmin.store.commit(
-                'store/setFavourites',
-                response.data.favourites
-            );
-        } catch (e) {
-            console.error(e);
-        }
-    }
-
-    isFavourite() {
-        return _.find(crudadmin.store.state.store.favourites, {
-            product_id: parseInt(this.id),
-        })
-            ? true
-            : false;
-    }
 }
 
 module.exports = Product;
