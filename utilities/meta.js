186e7d4424334f364a39e1733f357309934bea62
Marek Gogoľ <info@marekgogol.sk>
2022-04-28 14:05:29 +0200

    added default og image support

--- utilities/meta.js
+++ utilities/meta.js
@@ -1,4 +1,6 @@
-const sanitize = text => {
+const _ = require('lodash');
+
+const sanitize = (text) => {
     text = text || '';
 
     if (typeof text !== 'string') {
@@ -11,7 +13,7 @@ const sanitize = text => {
         .replace(new RegExp('&nbsp;', 'g'), ' ');
 };
 
-module.exports.bindModelMeta = (options, model) => {
+module.exports.bindModelMeta = (options, model, properties) => {
     let title = model.meta_title || model.name || model.title;
     if (title) {
         options.title = sanitize(title);
@@ -48,7 +50,12 @@ module.exports.bindModelMeta = (options, model) => {
         });
     }
 
-    let images = model.metaImageThumbnail || model.meta_image;
+    let images = model.metaImageThumbnail || model.meta_image || [];
+
+    if (properties.image && _.isArray(images)) {
+        images = images.concat([properties.image]);
+    }
+
     if (images && images.length > 0) {
         for (var i = 0; i < images.length; i++) {
             options.meta.push({
