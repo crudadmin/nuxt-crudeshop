407c0e2f1dda2443a5ae411aeb3b56530693c81a
Marek Gogoľ <info@marekgogol.sk>
2023-10-09 22:41:50 +0200

    fetch response test added

--- nuxt/composables/listing.js
+++ nuxt/composables/listing.js
@@ -3,3 +3,17 @@ import Product from '../models/Product.js';
 export const useToProductModels = (products) => {
     return products.map((item) => new Product(item));
 };
+
+export const useFetchProductDetail = async (slug) => {
+    let response = await useAxios().$get(
+        useAction('ProductController@show', slug)
+    );
+
+    return response;
+};
+
+export const useAsyncFetchProductDetail = (slug) => {
+    return useAsyncData('product.detail.' + slug, () => {
+        return useFetchProductDetail(slug);
+    });
+};
