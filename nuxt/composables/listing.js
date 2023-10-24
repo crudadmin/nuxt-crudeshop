d702b6e78437348355a578515088b2537e4c404c
Marek Gogoľ <info@marekgogol.sk>
2023-10-24 15:10:54 +0200

    product listing updated

--- nuxt/composables/listing.js
+++ nuxt/composables/listing.js
@@ -1,10 +1,17 @@
+import _ from 'lodash';
 import Product from '../models/Product.js';
 
 export const useToProductModels = (products) => {
-    return products.map((item) => new Product(item));
+    return products.map((item) => useToProductModel(item));
 };
 
-export const useFetchProductDetail = async (slug) => {
+export const useToProductModel = (product) => {
+    return new Product(product);
+};
+
+export const useFetchProductDetail = async (options) => {
+    const { slug } = options;
+
     let response = await useAxios().$get(
         useAction('ProductController@show', slug)
     );
@@ -12,8 +19,17 @@ export const useFetchProductDetail = async (slug) => {
     return response;
 };
 
-export const useAsyncFetchProductDetail = (slug) => {
-    return useAsyncData('product.detail.' + slug, () => {
-        return useFetchProductDetail(slug);
-    });
+export const useAsyncFetchProductDetail = async (options) => {
+    const { data } = await useAsyncData(
+        'product.detail.' + options.slug,
+        () => {
+            return useFetchProductDetail(options);
+        }
+    );
+
+    let response = _.cloneDeep(data.value);
+
+    response.model.product = useToProductModel(response.model.product);
+
+    return response;
 };
