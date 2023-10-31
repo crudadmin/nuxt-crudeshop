e9cf42abf7501dc14569af5306420f62593ff20c
Marek Gogoľ <info@marekgogol.sk>
2023-10-31 13:00:59 +0100

    fix + release

--- mixins/listing.mixin.js
+++ mixins/listing.mixin.js
@@ -17,7 +17,7 @@ const config = {
 const fetchListing = async (context, fetchOptions) => {
     const { store, route } = context;
 
-    let routeAction = store.getters['listing/getListingFetchRoute']()(context);
+    let routeAction = store.getters['listing/getListingFetchRoute'](context);
 
     store.commit('listing/setDefaultFetchRoute', routeAction);
 
